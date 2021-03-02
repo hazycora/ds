122eba11924c5f7784452f24d2becd71ecf804d8
hazycora <72580505+hazycora@users.noreply.github.com>
2021-03-02 02:40:07 -0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,32 @@
+var apps = [
+  {
+    line1: "DUMB GAME 1",
+    line2: "Stupid",
+    publisher: "Nintendo",
+    icon: "assets/icon.png"
+  },
+  {
+    line1: "DUMB GAME 2",
+    line2: "Stupid",
+    publisher: "Nintendo",
+    icon: "assets/icon.png"
+  },
+  {
+    line1: "DUMB GAME 3",
+    line2: "Stupid",
+    publisher: "Nintendo",
+    icon: "assets/icon.png"
+  },
+  {
+    line1: "DUMB GAME 4",
+    line2: "Stupid",
+    publisher: "Nintendo",
+    icon: "assets/icon.png"
+  },
+  {
+    line1: "DUMB GAME 5",
+    line2: "Stupid",
+    publisher: "Nintendo",
+    icon: "assets/icon.png"
+  }
+]
---
+++ script.js
@@ -0,0 +1,156 @@
+var currentScroll = 0;
+
+function scrollTap(event) {
+  var goTo = Math.max(((event.clientX-19-14.5)/5.3), 0)
+  goTo = Math.min(goTo, 48)
+  //window.location.href = "/?scroll="+Math.floor(goTo)
+  refresh(Math.floor(goTo))
+}
+
+function scrollTo(goTo) {
+  refresh(Math.floor(goTo))
+}
+
+function goLeft() {
+  refresh(Math.floor(currentScroll-1))
+}
+
+function goRight() {
+  refresh(Math.floor(currentScroll+1))
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function refresh(scroll) {
+  currentScroll = scroll
+  var perPage = Math.min(Math.max(parseInt(scroll)+3, 3), 5)
+  var amount = Math.min((perPage - scroll + 2), perPage)
+
+  if((parseInt(scroll)+1)<=apps.length) {
+    document.querySelector("#gameDetails").setAttribute("style", "");
+    document.querySelector("#gameDetails").innerHTML = apps[scroll].line1+"<br>"+apps[scroll].line2+"<br>"+apps[scroll].publisher;
+  }else{
+    document.querySelector("#gameDetails").setAttribute("style", "display:none;");
+  }
+
+
+  removeAllChildNodes(document.querySelector("#gameRow"))
+
+  var bracketClosed = document.createElement("div");
+  bracketClosed.setAttribute("class", "bracket");
+  bracketClosed.setAttribute("closed", "1");
+  var bracket = document.createElement("div");
+  bracket.setAttribute("class", "bracket");
+  var air = document.createElement("div");
+  air.setAttribute("class", "air");
+
+  if(scroll==0) {
+    document.querySelector("#gameRow").appendChild(air);
+  }
+  if(scroll<=1) {
+    document.querySelector("#gameRow").appendChild(bracket);
+  }
+
+  if(apps.length <= perPage) {
+    if(amount > 0) {
+      for (var index = 0; index < amount; index ++) {
+        if (index == perPage-3) {
+          var element = document.createElement("div");
+          element.setAttribute("class", "gameSelected");
+          var img = document.createElement("img");
+          img.setAttribute("class", "icon");
+          img.setAttribute("src", apps[scroll].icon);
+          element.appendChild(img);
+          document.querySelector("#gameRow").appendChild(element);
+        }else{
+          var element = document.createElement("div");
+          element.setAttribute("class", "game");
+          var img = document.createElement("img");
+          img.setAttribute("class", "icon");
+          img.setAttribute("src", apps[scroll].icon);
+          element.appendChild(img);
+          document.querySelector("#gameRow").appendChild(element);
+        }
+      }
+    }
+    var empty = document.createElement("div");
+    empty.setAttribute("class", "empty");
+    var emptySelected = document.createElement("div");
+    emptySelected.setAttribute("class", "emptySelected");
+
+    if(scroll>=47) {
+      if(scroll==48) {
+        document.querySelector("#gameRow").appendChild(empty);
+        document.querySelector("#gameRow").appendChild(empty.cloneNode(true));
+        document.querySelector("#gameRow").appendChild(emptySelected.cloneNode(true));
+
+        document.querySelector("#gameRow").appendChild(bracketClosed);
+
+        document.querySelector("#gameRow").appendChild(air);
+      }else {
+        document.querySelector("#gameRow").appendChild(empty);
+        document.querySelector("#gameRow").appendChild(empty.cloneNode(true));
+        document.querySelector("#gameRow").appendChild(emptySelected.cloneNode(true));
+        document.querySelector("#gameRow").appendChild(empty.cloneNode(true));
+
+        document.querySelector("#gameRow").appendChild(bracketClosed);
+      }
+    }else {
+      for (var index = 0; index < Math.min((perPage - amount), perPage); index ++) {
+        if (index == perPage-(3+Math.max(amount, 0))) {
+          document.querySelector("#gameRow").appendChild(emptySelected.cloneNode(true));
+        }else{
+          document.querySelector("#gameRow").appendChild(empty.cloneNode(true));
+        }
+
+      }
+    }
+  }else{
+    if(amount > 0) {
+      for (var index = 0; index < amount; index ++) {
+        if (index == perPage-3) {
+          var element = document.createElement("div");
+          element.setAttribute("class", "gameSelected");
+          var img = document.createElement("img");
+          img.setAttribute("class", "icon");
+          img.setAttribute("src", "assets/icon.png");
+          element.appendChild(img);
+          document.querySelector("#gameRow").appendChild(element);
+        }
+        if (index != perPage-3) {
+          var element = document.createElement("div");
+          element.setAttribute("class", "game");
+          var img = document.createElement("img");
+          img.setAttribute("class", "icon");
+          img.setAttribute("src", "assets/icon.png");
+          element.appendChild(img);
+          document.querySelector("#gameRow").appendChild(element);
+        }
+      }
+    }
+  }
+
+
+  if(scroll>0) {
+    document.querySelector("#leftButton").setAttribute("onclick", "goLeft()");
+  }else{
+    if(document.querySelector("#leftButton").hasAttribute("onclick")) {
+      document.querySelector("#leftButton").removeAttribute("onclick");
+    }
+  }
+
+  if((parseInt(scroll)+1)<=48) {
+    document.querySelector("#rightButton").setAttribute("onclick", "goRight()");
+  }else{
+    if(document.querySelector("#rightButton").hasAttribute("onclick")) {
+      document.querySelector("#rightButton").removeAttribute("onclick");
+    }
+  }
+
+
+  document.querySelector("#handle").setAttribute("style", "left: "+((scroll*5.3)+19)+"px;");
+}
