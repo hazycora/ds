ab7e35aeb299bb0e95256e4ea667be94a8ad2a89
hazycora <72580505+hazycora@users.noreply.github.com>
2021-03-02 02:56:52 -0600

    add links

--- apps.js
+++ apps.js
@@ -3,30 +3,35 @@ var apps = [
     line1: "DUMB GAME 1",
     line2: "Stupid",
     publisher: "Nintendo",
-    icon: "assets/icon.png"
+    icon: "assets/icon.png",
+    link: "https://hazycora.com"
   },
   {
     line1: "DUMB GAME 2",
     line2: "Stupid",
     publisher: "Nintendo",
-    icon: "assets/icon.png"
+    icon: "assets/icon.png",
+    link: "https://hazycora.com"
   },
   {
     line1: "DUMB GAME 3",
     line2: "Stupid",
     publisher: "Nintendo",
-    icon: "assets/icon.png"
+    icon: "assets/icon.png",
+    link: "https://hazycora.com"
   },
   {
     line1: "DUMB GAME 4",
     line2: "Stupid",
     publisher: "Nintendo",
-    icon: "assets/icon.png"
+    icon: "assets/icon.png",
+    link: "https://hazycora.com"
   },
   {
     line1: "DUMB GAME 5",
     line2: "Stupid",
     publisher: "Nintendo",
-    icon: "assets/icon.png"
+    icon: "assets/icon.png",
+    link: "https://hazycora.com"
   }
 ]
